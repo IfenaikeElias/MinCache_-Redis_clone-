ae9784e2758e2c0c7b7d7d95edd6495ef4353c3f
IfenaikeElias <elias.ifenaike@gmail.com>
2025-09-19 18:44:32 +0100

    implemeted INCR

--- src/main.go
+++ src/main.go
@@ -12,11 +12,15 @@ import (
 )
 
 const CRLF = "\r\n"
+ 
 
 type Server struct {
 	Listener net.Listener
 	Commands map[string]CommandHandler
+	
 }
+
+var DB sync.Map
 
 // ------- Connection helpers ----------
 
@@ -74,6 +78,7 @@ type GetHandler struct{}
 type RpushHandler struct{}
 type LrangeHandler struct{}
 type LpushHandler struct{}
+type IncrHandler struct{}
 
 type CommandHandler interface {
 	Execute(conn net.Conn, args ...string)
@@ -85,8 +90,10 @@ var commands = map[string]CommandHandler{
 	"SET":    SetHandler{},
 	"GET":    GetHandler{},
 	"RPUSH":  RpushHandler{},
-	"LPUSH": LpushHandler{},
+	"LPUSH":  LpushHandler{},
 	"LRANGE": LrangeHandler{},
+	"INCR": IncrHandler{},
+	
 }
 
 func (e EchoHandler) Execute(conn net.Conn, args ...string) {
@@ -105,62 +112,82 @@ func (p PingHandler) Execute(conn net.Conn, args ...string) {
 	}
 }
 
+func (i IncrHandler) Execute(conn net.Conn, args ...string){
+	if len(args) != 1 {
+		writeErr(conn, "INCR requires only one arguement")
+		return
+	}
+	key := args[0]
+	val, ok := DB.Load(key)
+	var curr int
+	if !ok {
+       curr = 0
+	   
+	} else {
+		bucket, ok := val.(Bucket)
+		if !ok {
+			writeErr(conn, "ERR Operation against wrong type")
+			return
+		}
+		i, err := strconv.Atoi(bucket.Val)
+		if err != nil {
+			writeErr(conn, "ERR value is not an integer")
+			return
+		}
+		curr = i
+	}
+	curr ++
+	DB.Store(key, Bucket{Val: strconv.Itoa(curr)})
+	writeStr()
+}
+
 type Bucket struct {
 	Val        string
 	PX         int       // TTL in ms
-	ExpiryTime time.Time // absolute expiry
+	ExpiryTime time.Time // expiry
 	HasExpiry  bool      // whether to expire
 }
 
-var DB sync.Map
-
 func (s SetHandler) Execute(conn net.Conn, args ...string) {
-	if len(args) < 2 {
-		writeErr(conn, "SET command needs at least 2 arguments: key and value")
-		return
-	}
-	if len(args) > 4 {
-		writeErr(conn, "SET command accepts at most 4 arguments")
-		return
-	}
+    if len(args) < 2 || len(args) > 4 {
+        writeErr(conn, "ERR wrong number of arguments for 'set' command")
+        return
+    }
+    key, value := args[0], args[1]
 
-	key, value := args[0], args[1]
+    var (
+        d         time.Duration
+        hasExpiry bool
+    )
+    if len(args) == 4 {
+        if strings.ToUpper(args[2]) != "PX" {
+            writeErr(conn, "ERR syntax error")
+            return
+        }
+        ms, err := strconv.Atoi(args[3])
+        if err != nil || ms <= 0 {
+            writeErr(conn, "ERR PX value must be a positive integer")
+            return
+        }
+        d = time.Duration(ms) * time.Millisecond
+        hasExpiry = true
+    }
 
-	var (
-		d         time.Duration
-		hasExpiry bool
-	)
-	if len(args) == 4 {
-		if strings.ToUpper(args[2]) != "PX" {
-			writeErr(conn, "SET command expects PX as third argument")
-			return
-		}
-		ms, err := strconv.Atoi(args[3])
-		if err != nil || ms <= 0 {
-			writeErr(conn, "PX value must be a positive integer")
-			return
-		}
-		d = time.Duration(ms) * time.Millisecond
-		hasExpiry = true
-	}
+    now := time.Now()
+    var bucket Bucket
+    if hasExpiry {
+        bucket = Bucket{
+            Val:        value,
+            PX:         int(d.Milliseconds()),
+            ExpiryTime: now.Add(d),
+            HasExpiry:  true,
+        }
+    } else {
+        bucket = Bucket{Val: value}
+    }
 
-	now := time.Now()
-	bucket := Bucket{
-		Val:        value,
-		PX:         int(d.Milliseconds()),
-		ExpiryTime: now.Add(d),
-		HasExpiry:  hasExpiry,
-	}
-
-	DB.Store(key, bucket)
-
-	if hasExpiry {
-		time.AfterFunc(d, func() {
-			DB.Delete(key)
-		})
-	}
-
-	writeStr(conn, "OK")
+    DB.Store(key, bucket)
+    writeStr(conn, "OK")
 }
 
 func (g GetHandler) Execute(conn net.Conn, args ...string) {
@@ -191,14 +218,13 @@ func (g GetHandler) Execute(conn net.Conn, args ...string) {
 func (r RpushHandler) Execute(conn net.Conn, args ...string) {
 	if len(args) < 2 {
 		writeErr(conn, "RPUSH requires at least 2 arguements")
-		return 
+		return
 	}
 	key := args[0]
 	items := args[1:]
 	value, _ := DB.LoadOrStore(key, []string{})
 	list, ok := value.([]string)
 	if !ok {
-	
 		writeErr(conn, "Invalid data type for the key")
 		return
 	}
@@ -207,10 +233,10 @@ func (r RpushHandler) Execute(conn net.Conn, args ...string) {
 	writeBulkStr(conn, fmt.Sprintf("(%d)", len(list)))
 }
 
-// ------ Utility function for Lpush to turn arounf list ------
+// ------ Utility function for Lpush to turn around list ------
 func reverse(s []string) []string {
-	i, j := 0, len(s) - 1
-	for i < j { 
+	i, j := 0, len(s)-1
+	for i < j {
 		s[i], s[j] = s[j], s[i]
 		i++
 		j--
@@ -310,14 +336,33 @@ func (s *Server) parseCommand(line string) (CommandHandler, []string, error) {
 	return handler, parts[1:], nil
 }
 
+func startReaper(interval time.Duration) {
+    ticker := time.NewTicker(interval)
+    go func() {
+        for now := range ticker.C {
+            DB.Range(func(k, v interface{}) bool {
+                b := v.(Bucket)
+                if b.HasExpiry && now.After(b.ExpiryTime) {
+                    DB.Delete(k)
+                }
+                return true
+            })
+        }
+    }()
+}
+
+
 // ----------- main ------------
 
 func main() {
+	startReaper(1 * time.Second)
 	s := &Server{
 		Commands: commands,
 	}
 	s.ListenForConn()
 	defer s.CloseConn()
+	fmt.Println("Listening on 0.0.0.0:6379")
+
 	for {
 		conn, err := s.AcceptConn()
 		if err != nil {
@@ -339,4 +384,3 @@ func main() {
 		}(conn)
 	}
 }
- 
